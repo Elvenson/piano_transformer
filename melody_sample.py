57acc86cc2049b97f1d02e70cb9176d9cef5fb47
Quoc Bao Bui <bao.bui@shopee.com>
2020-07-04 13:50:04 +0800

    Fix Pylint.

--- melody_sample.py
+++ melody_sample.py
@@ -3,140 +3,144 @@
 # Modification copyright 2020 Bui Quoc Bao
 # Change notebook script into package
 
-from utils import *
-
-from tensor2tensor.utils import decoding
+"""Conditioned Transformer."""
 from tensor2tensor.utils import trainer_lib
+from tensor2tensor.utils import decoding
 
-flags = tf.compat.v1.flags
+import utils
+
+flags = utils.tf.compat.v1.flags
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string(
-	'log', 'INFO',
-	'The threshold for what messages will be logged: '
-	'DEBUG, INFO, WARN, ERROR, or FATAL.'
+    'log', 'INFO',
+    'The threshold for what messages will be logged: '
+    'DEBUG, INFO, WARN, ERROR, or FATAL.'
 )
 flags.DEFINE_string(
-	'model_name', 'transformer',
-	'The pre-trained model for sampling.'
+    'model_name', 'transformer',
+    'The pre-trained model for sampling.'
 )
 flags.DEFINE_string(
-	'hparams_set', 'transformer_tpu',
-	'Set of hparams to use.'
+    'hparams_set', 'transformer_tpu',
+    'Set of hparams to use.'
 )
 flags.DEFINE_string(
-	'model_path', None,
-	'Pre-trained model path.'
+    'model_path', None,
+    'Pre-trained model path.'
 )
 flags.DEFINE_string(
-	'melody_path', None,
-	'Midi file path for melody'
+    'melody_path', None,
+    'Midi file path for melody'
 )
 flags.DEFINE_string(
-	'output', None,
-	'Midi output path.'
+    'output', None,
+    'Midi output path.'
 )
 flags.DEFINE_string(
-	'sample', 'random',
-	'Sampling method.'
+    'sample', 'random',
+    'Sampling method.'
 )
 flags.DEFINE_integer(
-	'layers', 16,
-	'Number of hidden layers.'
+    'layers', 16,
+    'Number of hidden layers.'
 )
 flags.DEFINE_integer(
-	'beam_size', 1,
-	'Beam size for inference.'
+    'beam_size', 1,
+    'Beam size for inference.'
 )
 flags.DEFINE_integer(
-	'decode_length', 1024,
-	'Length of decode result.'
+    'decode_length', 1024,
+    'Length of decode result.'
 )
 flags.DEFINE_float(
-	'alpha', 0.0,
-	'Alpha for decoder.'
+    'alpha', 0.0,
+    'Alpha for decoder.'
 )
 
 
 def run():
-	"""
-	Load Transformer model according to flags and start sampling.
-	:raises:
-		ValueError: if required flags are missing or invalid.
-	"""
-	if FLAGS.model_path is None:
-		raise ValueError(
-			'Required Transformer pre-trained model path.'
-		)
+    """
+    Load Transformer model according to flags and start sampling.
+    :raises:
+        ValueError: if required flags are missing or invalid.
+    """
+    if FLAGS.model_path is None:
+        raise ValueError(
+            'Required Transformer pre-trained model path.'
+        )
 
-	if FLAGS.output is None:
-		raise ValueError(
-			'Required Midi output path.'
-		)
+    if FLAGS.output is None:
+        raise ValueError(
+            'Required Midi output path.'
+        )
 
-	if FLAGS.decode_length <= 0:
-		raise ValueError(
-			'Decode length must be > 0.'
-		)
+    if FLAGS.decode_length <= 0:
+        raise ValueError(
+            'Decode length must be > 0.'
+        )
 
-	if FLAGS.melody_path is None:
-		raise ValueError(
-			'Required melody Midi file path.'
-		)
+    if FLAGS.melody_path is None:
+        raise ValueError(
+            'Required melody Midi file path.'
+        )
 
-	problem = MelodyToPianoPerformanceProblem()
-	melody_conditioned_encoders = problem.get_feature_encoders()
-	melody_ns = get_melody_ns(FLAGS.melody_path)
-	inputs = melody_conditioned_encoders['inputs'].encode_note_sequence(
-		melody_ns)
+    problem = utils.MelodyToPianoPerformanceProblem()
+    melody_conditioned_encoders = problem.get_feature_encoders()
+    melody_ns = utils.get_melody_ns(FLAGS.melody_path)
+    inputs = melody_conditioned_encoders['inputs'].encode_note_sequence(
+        melody_ns)
 
-	# Set up HParams.
-	hparams = trainer_lib.create_hparams(hparams_set=FLAGS.hparams_set)
-	trainer_lib.add_problem_hparams(hparams, problem)
-	hparams.num_hidden_layers = FLAGS.layers
-	hparams.sampling_method = FLAGS.sample
+    # Set up HParams.
+    hparams = trainer_lib.create_hparams(hparams_set=FLAGS.hparams_set)
+    trainer_lib.add_problem_hparams(hparams, problem)
+    hparams.num_hidden_layers = FLAGS.layers
+    hparams.sampling_method = FLAGS.sample
 
-	# Set up decoding HParams.
-	decode_hparams = decoding.decode_hparams()
-	decode_hparams.alpha = FLAGS.alpha
-	decode_hparams.beam_size = FLAGS.beam_size
+    # Set up decoding HParams.
+    decode_hparams = decoding.decode_hparams()
+    decode_hparams.alpha = FLAGS.alpha
+    decode_hparams.beam_size = FLAGS.beam_size
 
-	# Create Estimator.
-	logging.info('Loading model')
-	run_config = trainer_lib.create_run_config(hparams)
-	estimator = trainer_lib.create_estimator(
-		FLAGS.model_name, hparams, run_config,
-		decode_hparams=decode_hparams
-	)
+    # Create Estimator.
+    utils.LOGGER.info('Loading model')
+    run_config = trainer_lib.create_run_config(hparams)
+    estimator = trainer_lib.create_estimator(
+        FLAGS.model_name, hparams, run_config,
+        decode_hparams=decode_hparams
+    )
 
-	# Start the Estimator, loading from the specified checkpoint.
-	input_fn = decoding.make_input_fn_from_generator(melody_input_generator(inputs, FLAGS.decode_length))
-	melody_conditioned_samples = estimator.predict(
-		input_fn, checkpoint_path=FLAGS.model_path)
+    # Start the Estimator, loading from the specified checkpoint.
+    input_fn = decoding.make_input_fn_from_generator(utils.melody_input_generator(
+        inputs, FLAGS.decode_length))
+    melody_conditioned_samples = estimator.predict(
+        input_fn, checkpoint_path=FLAGS.model_path)
 
-	# Generate sample events.
-	logging.info('Generating sample.')
-	sample_ids = next(melody_conditioned_samples)['outputs']
+    # Generate sample events.
+    utils.LOGGER.info('Generating sample.')
+    sample_ids = next(melody_conditioned_samples)['outputs']
 
-	# Decode to NoteSequence.
-	logging.info('Decoding sample id')
-	midi_filename = decode(
-		sample_ids,
-		encoder=melody_conditioned_encoders['targets'])
-	accompaniment_ns = mm.midi_file_to_note_sequence(midi_filename)
+    # Decode to NoteSequence.
+    utils.LOGGER.info('Decoding sample id')
+    midi_filename = utils.decode(
+        sample_ids,
+        encoder=melody_conditioned_encoders['targets'])
+    accompaniment_ns = utils.mm.midi_file_to_note_sequence(midi_filename)
 
-	logging.info('Converting note sequences to Midi file.')
-	mm.sequence_proto_to_midi_file(accompaniment_ns, FLAGS.output)
+    utils.LOGGER.info('Converting note sequences to Midi file.')
+    utils.mm.sequence_proto_to_midi_file(accompaniment_ns, FLAGS.output)
 
 
 def main(unused_argv):
-	logging.set_verbosity(FLAGS.log)
-	run()
+    """Invoke run function, set log level."""
+    utils.LOGGER.set_verbosity(FLAGS.log)
+    run()
 
 
 def console_entry_point():
-	tf.compat.v1.app.run(main)
+    """Call main function."""
+    utils.tf.compat.v1.app.run(main)
 
 
 if __name__ == '__main__':
-	console_entry_point()
+    console_entry_point()
--- utils.py
+++ utils.py
@@ -3,109 +3,112 @@
 # Modification copyright 2020 Bui Quoc Bao
 # Change notebook script into package
 
+"""Transform utilities."""
 import numpy as np
 import tensorflow as tf
 
 from tensor2tensor.data_generators import text_encoder
 
-
 import magenta.music as mm
 from magenta.models.score2perf import score2perf
 
-logging = tf.compat.v1.logging
+LOGGER = tf.compat.v1.logging
 
 
-class PianoPerformanceLanguageModelProblem(score2perf.Score2PerfProblem):
-	@property
-	def add_eos_symbol(self):
-		return True
+class PianoPerformanceLanguageModelProblem(score2perf.Score2PerfProblem):   # pylint: disable=missing-module-docstring, abstract-method, missing-class-docstring
+    @property
+    def add_eos_symbol(self):
+        return True
 
 
-class MelodyToPianoPerformanceProblem(score2perf.AbsoluteMelody2PerfProblem):
-	@property
-	def add_eos_symbol(self):
-		return True
+class MelodyToPianoPerformanceProblem(score2perf.AbsoluteMelody2PerfProblem):   # pylint: disable=missing-module-docstring, abstract-method, missing-class-docstring
+    @property
+    def add_eos_symbol(self):
+        return True
 
 
 def decode(ids, encoder):
-	"""Decode a list of IDs."""
-	ids = list(ids)
-	if text_encoder.EOS_ID in ids:
-		ids = ids[:ids.index(text_encoder.EOS_ID)]
-	return encoder.decode(ids)
+    """Decode a list of IDs."""
+    ids = list(ids)
+    if text_encoder.EOS_ID in ids:
+        ids = ids[:ids.index(text_encoder.EOS_ID)]
+    return encoder.decode(ids)
 
 
 def unconditional_input_generator(targets, decode_length):
-	"""Estimator input function for unconditional Transformer."""
-	while True:
-		yield {
-			'targets': np.array([targets], dtype=np.int32),
-			'decode_length': np.array(decode_length, dtype=np.int32)
-		}
+    """Estimator input function for unconditional Transformer."""
+    while True:
+        yield {
+            'targets': np.array([targets], dtype=np.int32),
+            'decode_length': np.array(decode_length, dtype=np.int32)
+        }
 
 
 def melody_input_generator(inputs, decode_length):
-	"""Estimator input function for melody Transformer."""
-	while True:
-		yield {
-			'inputs': np.array([[inputs]], dtype=np.int32),
-			'targets': np.zeros([1, 0], dtype=np.int32),
-			'decode_length': np.array(decode_length, dtype=np.int32)
-		}
+    """Estimator input function for melody Transformer."""
+    while True:
+        yield {
+            'inputs': np.array([[inputs]], dtype=np.int32),
+            'targets': np.zeros([1, 0], dtype=np.int32),
+            'decode_length': np.array(decode_length, dtype=np.int32)
+        }
 
 
 def get_primer_ns(filename, max_length):
-	"""
-	Convert Midi file to note sequences for priming.
-	:param filename: Midi file name.
-	:param max_length: Maximum note sequence length for priming in seconds.
-	:return:
-		Note sequences for priming.
-	"""
-	primer_ns = mm.midi_file_to_note_sequence(filename)
+    """
+    Convert Midi file to note sequences for priming.
+    :param filename: Midi file name.
+    :param max_length: Maximum note sequence length for priming in seconds.
+    :return:
+        Note sequences for priming.
+    """
+    primer_ns = mm.midi_file_to_note_sequence(filename)
 
-	# Handle sustain pedal in primer.
-	primer_ns = mm.apply_sustain_control_changes(primer_ns)
+    # Handle sustain pedal in primer.
+    primer_ns = mm.apply_sustain_control_changes(primer_ns)
 
-	# Trim to desired number of seconds.
-	if primer_ns.total_time > max_length:
-		logging.warn('Primer is longer than %d seconds, truncating.')
-		primer_ns = mm.extract_subsequence(
-			primer_ns, 0, max_length
-		)
+    # Trim to desired number of seconds.
+    if primer_ns.total_time > max_length:
+        LOGGER.warn(
+            'Primer duration %d is longer than max second %d, truncating.'
+            % (primer_ns.total_time, max_length))
+        primer_ns = mm.extract_subsequence(
+            primer_ns, 0, max_length
+        )
 
-	# Remove drums from primer if present.
-	if any(note.is_drum for note in primer_ns.notes):
-		logging.warn('Primer contains drums; they will be removed.')
-		notes = [note for note in primer_ns.notes if not note.is_drum]
-		del primer_ns.notes[:]
-		primer_ns.notes.extend(notes)
+    # Remove drums from primer if present.
+    if any(note.is_drum for note in primer_ns.notes):
+        LOGGER.warn('Primer contains drums; they will be removed.')
+        notes = [note for note in primer_ns.notes if not note.is_drum]
+        del primer_ns.notes[:]
+        primer_ns.notes.extend(notes)
 
-	# Set primer instrument and program.
-	for note in primer_ns.notes:
-		note.instrument = 1
-		note.program = 0
+    # Set primer instrument and program.
+    for note in primer_ns.notes:
+        note.instrument = 1
+        note.program = 0
 
-	return primer_ns
+    return primer_ns
 
 
 def get_melody_ns(filename):
-	"""
-	Convert melody Midi file to note sequence.
-	:param filename: Midi file name.
-	:return:
-		Melody note sequences.
-	"""
-	melody_ns = mm.midi_file_to_note_sequence(filename)
-	melody_instrument = mm.infer_melody_for_sequence(melody_ns)
-	notes = [note for note in melody_ns.notes if note.instrument == melody_instrument]
-	del melody_ns.notes[:]
-	melody_ns.notes.extend(
-		sorted(notes, key=lambda note: note.start_time)
-	)
-	for i in range(len(melody_ns.notes) - 1):
-		melody_ns.notes[i].end_time = melody_ns.notes[i+1].start_time
+    """
+    Convert melody Midi file to note sequence.
+    :param filename: Midi file name.
+    :return:
+        Melody note sequences.
+    """
+    melody_ns = mm.midi_file_to_note_sequence(filename)
+    melody_instrument = mm.infer_melody_for_sequence(melody_ns)
+    # pylint: disable=no-member
+    notes = [note for note in melody_ns.notes if note.instrument == melody_instrument]
+    del melody_ns.notes[:]
+    melody_ns.notes.extend(
+        sorted(notes, key=lambda note: note.start_time)
+    )
+    for i in range(len(melody_ns.notes) - 1):
+        melody_ns.notes[i].end_time = melody_ns.notes[i + 1].start_time
 
-	return melody_ns
+    # pylint: disable=no-member
 
-
+    return melody_ns
